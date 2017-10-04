e20eb64cb8caed50e3f4c4895e2b0165850a76d4
Bo X Chen <bchen2@compute-0-36.local>
2017-10-03 20:01:59 -0400

    refactor code

--- charnn/charnn.py
+++ charnn/charnn.py
@@ -9,7 +9,7 @@ import time
 from model import CharNN
 
 def load_file(filename):
-  content = "" 
+  content = ""
   charset = set()
 
   with open(filename) as f:
@@ -18,40 +18,35 @@ def load_file(filename):
       content += line
   ch2ix = {ch: ix for ix, ch in enumerate(charset)}
   ix2ch = {ix: ch for ix, ch in enumerate(charset)}
-  return content, ch2ix, ix2ch 
+  return content, ch2ix, ix2ch
 
 def sample():
   #TODO: replace with beam search
   ix = random.randint(0, input_size-1)
   line = ""
   line += ix2ch[ix]
-  h = (Variable(torch.zeros(args.nlayers, 1, args.hidden_size).cuda()),
-      Variable(torch.zeros(args.nlayers, 1, args.hidden_size).cuda()))
+  h = None
   for i in range(100):
-    prob, h = model(Variable(torch.LongTensor([[ix]])).cuda(), h)
+    prob, h = model(Variable(torch.LongTensor([[ix]])), h)
     prob = np.exp(prob.cpu().data.numpy().ravel())
     ix = np.random.choice(input_size, p = prob)
-    # ix = int(np.argmax(prob))
     line += ix2ch[ix]
   return line
 
 def random_sample():
-  samples = torch.LongTensor(args.batch_size, args.chunk_len).cuda()
-  targets = torch.LongTensor(args.batch_size, args.chunk_len).cuda()
+  samples = torch.LongTensor(args.batch_size, args.chunk_len)
+  targets = torch.LongTensor(args.batch_size, args.chunk_len)
   ndata = len(content)
   for i in range(args.batch_size):
     pos = random.randint(0, ndata - args.chunk_len - 1)
-    samples[i,:] = torch.LongTensor(
-        [ch2ix[ch] for ch in content[pos: pos + args.chunk_len]])
-    targets[i,:] = torch.LongTensor(
-        [ch2ix[ch] for ch in content[pos+1 : pos+args.chunk_len+1]])
-  # print(samples)
+    idxes = [ch2ix[ch] for ch in content[pos: pos + args.chunk_len + 1]]
+    samples[i,:] = torch.LongTensor(idxes[:-1])
+    targets[i,:] = torch.LongTensor(idxes[1:])
   return Variable(samples), Variable(targets)
 
 def train():
-  nsamples_per_epoch = len(content) // (args.chunk_len * args.batch_size)
-  # nsamples_per_epoch = 10
   criterion = nn.NLLLoss()
+  nsamples_per_epoch = len(content) // (args.chunk_len * args.batch_size)
   optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
   for e in range(args.nepochs):
@@ -63,29 +58,30 @@ def train():
       loss = 0
       for t in range(args.chunk_len):
         xout, h = model(samples[:, t], h)
-        loss += criterion(xout, targets[:, t])
+        loss += criterion(xout, targets[:, t].cuda())
       model.zero_grad()
       loss.backward()
       optimizer.step()
       epoch_loss += loss.data[0]
     print("Epoch %d, nll: %.4f, %.2f sec" % (e, epoch_loss, time.time() - ts))
-    print("Sample: [%s]\n" % sample())
+    if e % args.print_every == 0:
+      print("Sample: [%s]\n" % sample())
     sys.stdout.flush()
 
 if __name__ == "__main__":
   argparser = argparse.ArgumentParser()
   argparser.add_argument('filename', type=str)
   argparser.add_argument('--nepochs', type=int, default=2000)
-  # argparser.add_argument('--print_every', type=int, default=100)
+  argparser.add_argument('--print_every', type=int, default=10)
   argparser.add_argument('--nlayers', type=int, default=2)
   argparser.add_argument('--learning_rate', type=float, default=0.001)
   argparser.add_argument('--chunk_len', type=int, default=200)
   argparser.add_argument('--batch_size', type=int, default=400)
   argparser.add_argument('--hidden_size', type=int, default=100)
-  # argparser.add_argument('--cuda', action='store_true')
   args = argparser.parse_args()
 
   content, ch2ix, ix2ch = load_file(args.filename)
   input_size = len(ch2ix)
-  model = CharNN(args.nlayers, input_size, args.hidden_size, args.batch_size)
+  model = CharNN(args.nlayers, input_size, args.hidden_size, args.batch_size).cuda()
+  print(model)
   train()
--- charnn/model.py
+++ charnn/model.py
@@ -9,26 +9,22 @@ class CharNN(nn.Module):
     self.hidden_size = hidden_size
     self.nlayers = nlayers
 
-    self.embed = nn.Embedding(input_size, hidden_size).cuda()
-    self.h0 = Variable(torch.zeros(nlayers, batch_size, hidden_size).cuda())
-    self.c0 = Variable(torch.zeros(nlayers, batch_size, hidden_size).cuda())
-    # self.h0 = torch.zeros(nlayers, batch_size, hidden_size).cuda()
-    # self.c0 = torch.zeros(nlayers, batch_size, hidden_size).cuda()
+    self.embed = nn.Embedding(input_size, hidden_size)
     self.rnn = nn.LSTM(input_size = self.hidden_size,
-        hidden_size = self.hidden_size,
-        num_layers = self.nlayers).cuda()
-    self.fc = nn.Linear(hidden_size, input_size).cuda()
-    self.log_softmax = nn.LogSoftmax().cuda()
+        hidden_size = self.hidden_size, num_layers = self.nlayers)
+    self.fc = nn.Linear(hidden_size, input_size)
+    self.log_softmax = nn.LogSoftmax()
 
   def forward(self, xin, h):
     """
     xin: (batch_size, input_size)
     """
-    xin = self.embed(xin)
+    xin = self.embed(xin.cuda())
     batch_size = xin.size(0)
     if h is None:
-      h = (self.h0, self.c0)
+      h = (Variable(torch.zeros(self.nlayers, batch_size, self.hidden_size).cuda()),
+          Variable(torch.zeros(self.nlayers, batch_size, self.hidden_size).cuda()))
     xout, h = self.rnn(xin.view(1, batch_size, -1), h)
     xout = self.fc(xout.view(batch_size, -1))
     xout = self.log_softmax(xout)
-    return xout, h 
+    return xout, h
