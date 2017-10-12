add l2 normalization, fix log softmax bug in sampling

textsum/main.py
@@ -111,7 +111,10 @@ def train(data):
     if args.use_cuda:
         s2s = s2s.cuda()
     print(s2s)
-    s2s_opt = torch.optim.Adam(s2s.parameters(), lr=args.learning_rate)
+    for a in s2s.parameters():
+        print(a.data.size())
+    s2s_opt = torch.optim.Adam(s2s.parameters(), lr=args.learning_rate,
+            weight_decay = args.l2)
     identifier = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
     print("identifier:", identifier)
 
@@ -200,6 +203,7 @@ if __name__ == "__main__":
     argparser.add_argument('--batch_size', type=int, default=32)
     argparser.add_argument('--emb_size', type=int, default=128)
     argparser.add_argument('--hidden_size', type=int, default=128)
+    argparser.add_argument('--proj_size', type=int, default=128)
     argparser.add_argument('--vocab_size', type=int, default=50000)
     argparser.add_argument('--nlayers', type=int, default=1)
     argparser.add_argument('--nepochs', type=int, default=20)
@@ -207,7 +211,8 @@ if __name__ == "__main__":
     argparser.add_argument('--max_text_len', type=int, default=64)
     argparser.add_argument('--learning_rate', type=float, default=0.003)
     argparser.add_argument('--teach_ratio', type=float, default=0.5)
-    argparser.add_argument('--max_norm', type=float, default=100.0)
+    # argparser.add_argument('--max_norm', type=float, default=100.0)
+    argparser.add_argument('--l2', type=float, default=0.01)
 
     args = argparser.parse_args()
     for k, v in args.__dict__.items():

textsum/model/DecoderRNN.py
@@ -5,18 +5,21 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class DecoderRNN(nn.Module):
-    def __init__(self, vocab_size, emb, hidden_size, nlayers, teach_ratio):
+    def __init__(self, vocab_size, emb, hidden_size, proj_size, nlayers,
+            teach_ratio):
         super(DecoderRNN, self).__init__()
         self.nlayers = nlayers
         self.output_size = vocab_size
         self.hidden_size = hidden_size
+        self.proj_size = proj_size
         self.teach_ratio = teach_ratio
 
         self.emb = emb
         emb_size = self.emb.weight.size(1)
         self.rnn = nn.GRU(input_size=emb_size, hidden_size = hidden_size,
                 num_layers = nlayers, batch_first = True)
-        self.out = nn.Linear(self.hidden_size, self.output_size)
+        # self.l1 = nn.Linear(self.hidden_size, self.proj_size)
+        self.out = nn.Linear(self.proj_size, self.output_size)
 
     def forward(self, target, encoder_hidden):
         batch_size, max_seq_len = target.size()
@@ -29,6 +32,8 @@ class DecoderRNN(nn.Module):
         for t in range(1, max_seq_len):
             input_emb = self.emb(batch_input).unsqueeze(1)
             rnn_output, h = self.rnn(input_emb, h)
+            # xl1 = F.relu(self.l1(rnn_output))
+            # xout = self.out(xl1).squeeze(1)
             xout = self.out(rnn_output).squeeze(1)
             logp = F.log_softmax(xout)
             batch_output.append(logp)
@@ -53,8 +58,10 @@ class DecoderRNN(nn.Module):
         for t in range(1, max_seq_len):
             input_emb = self.emb(batch_input).unsqueeze(1)
             rnn_output, h = self.rnn(input_emb, h)
-            xout = self.out(rnn_output)
-            logp = F.log_softmax(xout).squeeze(1)
+            # xl1 = F.relu(self.l1(rnn_output))
+            # xout = self.out(xl1).squeeze(1)
+            xout = self.out(rnn_output).squeeze(1)
+            logp = F.log_softmax(xout)
             batch_output.append(logp)
 
             _, batch_input = torch.max(logp, 1, keepdim=False)

textsum/model/Seq2Seq.py
@@ -10,6 +10,7 @@ class Seq2Seq(nn.Module):
         self.emb_size = args.emb_size
         self.nlayers = args.nlayers
         self.hidden_size = args.hidden_size
+        self.proj_size = args.proj_size
         self.teach_ratio = args.teach_ratio
         self.max_title_len = args.max_title_len
         self.use_cuda = args.use_cuda
@@ -19,7 +20,7 @@ class Seq2Seq(nn.Module):
         self.encoder = EncoderRNN(self.vocab_size, self.emb, self.hidden_size,
                 self.nlayers)
         self.decoder = DecoderRNN(self.vocab_size, self.emb, self.hidden_size,
-                self.nlayers, self.teach_ratio)
+                self.proj_size, self.nlayers, self.teach_ratio)
 
     def forward(self, inputs, input_lens, targets):
         encoder_output, encoder_hidden = self.encoder(inputs, input_lens)